f80b827ce1c91f42460a2964adffaa1ceba3ff18
liamsnibachi <smail.chibani@soat.fr>
2014-11-22 11:56:45 +0100

    Ajout ligne 6

--- test.java
+++ test.java
@@ -2,9 +2,7 @@ J'ai modifié la ligne 11
 Modification ligne 22
 78
 J'ai ajouté une ligne 44
-<<<<<<< HEAD
 Modification en local 5
-=======
 Modification en local
 Ligne 6
->>>>>>> Ajout ligne 6
+Ajout ligne 66
